79743f217d430a19def6104cc4ba974c0b8d7521
asbilim <bilimdepaullilian@gmail.com>
2025-10-05 15:33:46 +0100

    basic structure done

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+export interface HealthDto {
+  status: string;
+  timestamp: string;
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): HealthDto {
+    return { status: 'OK', timestamp: new Date().toISOString() };
   }
 }
